5031446360a709b5ad6070f2aed2c9887b13b2c6
waghsunita <80938197+waghsunita@users.noreply.github.com>
2022-03-01 05:35:26 -0500

    Add files via upload

---
+++ con1.php
@@ -0,0 +1,16 @@
+<?php
+   $host        = "host =127.0.0.1";
+   $port        = "port =5432";
+   $dbname      = "dbname =test";
+   $credentials = "user=postgres password=postgres";
+
+   $db = pg_connect("$host $port $dbname $credentials");
+   if(!$db) 
+   {
+      echo "Error : Unable to open database\n";
+   }
+    else
+   {
+      echo "Opened database successfully\n";
+   }
+?>
